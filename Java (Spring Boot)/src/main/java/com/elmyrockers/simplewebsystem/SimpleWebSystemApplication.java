85b738ac6efb2f75557f95f47e828031dff95fa1
elmyrockers <elmyrockers@gmail.com>
2025-04-05 06:53:15 +0800

    Add Java (Spring Boot) source code

---
+++ Java (Spring Boot)/src/main/java/com/elmyrockers/simplewebsystem/SimpleWebSystemApplication.java
@@ -0,0 +1,13 @@
+package com.elmyrockers.simplewebsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleWebSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleWebSystemApplication.class, args);
+	}
+
+}
---
+++ Java (Spring Boot)/src/test/java/com/elmyrockers/simplewebsystem/SimpleWebSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elmyrockers.simplewebsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleWebSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
